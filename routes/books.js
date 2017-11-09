ecea15d4f9718f4f41be96a9a9a4d96eb2499445
Ellis Mwekunda <Scareb15@gmail.com>
2017-11-09 14:02:44 -0700

    added routes for request

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,34 @@ const express = require('express')
 const router = express.Router();
 const db = require('../db/query')
 
-router.get('/books', (req, res) => {
-    res.render('books')
+router.get('/', (req, res) => {
+    db.getAllBooks()
+    .then(books => res.render('books', {
+        books:books
+    }))
+})
+
+router.get('/new', (req, res) => {
+    res.render('new_book')
+})
+
+router.get('/delete/:id', (req, res) => {
+    let id = req.params.id
+    db.findBooktoDelete(id)
+    .then(books => res.render('delete_book', {
+        book: books[0]
+    }))
+})
+
+router.post('/', (req, res) => {
+    db.postNewBook(req.body)
+    .then(data => res.redirect('/books'))
+})
+
+router.delete('/:id', (req, res) => {
+    let id = req.params.id
+    db.deleteBook(id)
+    .then(() => res.redirect('/books'))
 })
 
 module.exports = router
